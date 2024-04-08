05d42cc84cc13d375f74292e2c27e699f7e5e538
Saif Ahmad <mdsaifahmad78@gmail.com>
2024-04-08 16:26:25 +0530

    new massage

---
+++ JavaScript_01.js
@@ -0,0 +1,74 @@
+// using console.log
+
+let name = "SheCodes";
+console.log("Hello " + name + "!");
+
+// what is vairables
+// The var statement declares a variable.
+// Variables are containers for storing information.
+// Creating a variable in JavaScript is called "declaring" a variable:
+
+var carName = "Volvo";
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+
+// Javascript operators are used to perform different types of mathematical and logical computations.
+// Examples:
+// The Assignment Operator = assigns values
+// The Addition Operator + adds values
+// The Multiplication Operator * multiplies values
+// The Comparison Operator > compares values
+// Operator	Description
+
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
+
+
+
+
+
